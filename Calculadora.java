26aa846291f70172d00c6f15f7cef5e05d0ef13c
aelm819 <taalib0101@gmail.com>
2026-03-06 11:36:51 +0100

    Commit Inicial

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    
+    private int numero1;
+    private int numero2;    
+
+    public Calculadora(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int sumar() {
+        return numero1 + numero2;
+    }
+}
